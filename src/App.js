39c22ba54a3f1dfe4ae2b01b4f9e5885623414e9
eniskrt <enskerti@gmail.com>
2024-01-24 23:26:31 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import { useState } from "react";
-import MainCard from "./components/main-card/MainCard";
-import SuccessCard from "./components/success-card/SuccessCard";
 import "./style.scss";
-import { Container } from "react-bootstrap";
 import AppRouter from "./routes";
 
-
-
 function App() {
-  
-  const [email, setEmail] = useState("")
 
   return (
     <AppRouter/>
